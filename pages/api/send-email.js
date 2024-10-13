7755c6747ddc815f2b5458cf36ebc906e38b95e4
Richard Holway <rgholway@gmail.com>
2024-10-13 17:42:32 -0400

    Add form

---
+++ pages/api/send-email.js
@@ -0,0 +1,23 @@
+import sendgrid from "@sendgrid/mail"
+
+export default async function handler(req, res) {
+  sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+  const { name, email, message, phoneNumber } = req.body
+  const msg = {
+    to: process.env.SENGRID_EMAIL,
+    from: "rgholway@gmail.com",
+    templateId: "d-0f3ef97e4b7446738a7c74444ae3254a",
+    dynamicTemplateData: {
+      name: name,
+      email: email,
+      phoneNumber: phoneNumber,
+      message: message
+    },
+  }
+  try {
+    await sendgrid.send(msg)
+    res.status(200).json({ message: 'Email sent successfully!' });
+  } catch (error) {
+    res.status(400).json({ message: 'Email did not send' });
+  }
+}
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,60 @@
+import { useForm } from "react-hook-form"
+import styles from '../styles/Home.module.css';
+import Links from "../components/Links";
+
 function Contact() {
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
+  const onSubmit = async (data) => {
+    console.log(data)
+    const response = fetch('/api/send-email', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+    const result = await response
+    if (result.ok) {
+      alert("Email sent successfully!");
+    }
+    console.log(result)
+  }
+
   return (
-    <div> Hello</div>
+    <div className={styles.container}>
+      <Links />
+      <div className={styles.textContainer}>
+        <h1>
+          If you are interested in supporting our efforts or have any questions please complete the form below.
+        </h1>
+
+        <div className={styles.formContainer}>
+          <form onSubmit={handleSubmit(onSubmit)} className={styles.styledForm}>
+            <div className={styles.formGroup}>
+              <label className={styles.formLabel}>Name</label>
+              <input className={styles.formInput} {...register("name", { required: true })} />
+              {errors.name && <p className={styles.errorMessage}>This field is required</p>}
+            </div>
+            <div className={styles.formGroup}>
+              <label className={styles.formLabel}>Email</label>
+              <input className={styles.formInput} {...register("email", { required: true })} />
+              {errors.email && <p className={styles.errorMessage}>This field is required</p>}
+            </div>
+            <div className={styles.formGroup}>
+              <label className={styles.formLabel}>Phone Number</label>
+              <input className={styles.formInput} {...register("phoneNumber")} />
+            </div>
+            <div className={styles.formGroup}>
+              <label className={styles.formLabel}>Message</label>
+              <textarea className={styles.formTextArea} {...register("message")} />
+            </div>
+            <button type="submit" className={styles.submitButton}>Send</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default Contact
